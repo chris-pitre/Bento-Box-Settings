15fcc896316f4bb7b2bfb4c5f3922b68fddc2b8f
Chris-Pitre <c.tophanp3@gmail.com>
2024-04-09 23:19:39 -0400

    changed numbers to numpad numbers

--- keymap.c
+++ keymap.c
@@ -18,11 +18,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * ----------------------------------------------------------------
      */
     [0] = LAYOUT(
-                                             KC_MUTE,     KC_MPLY,
-                   KC_MUTE,                 KC_PAUSE,      KC_INS,
-        KC_7,         KC_8,       KC_9,
-        KC_4,         KC_5,       KC_6,   S(KC_LBRC),  S(KC_RBRC),
-        KC_1,         KC_2,       KC_3,         KC_0,      KC_ENT
+                                                 KC_MUTE,     KC_MPLY,
+                   KC_MUTE,                     KC_PAUSE,      KC_INS,
+        KC_P7,         KC_P8,       KC_P9,
+        KC_P4,         KC_P5,       KC_P6,       KC_NLCK,     KC_PSCR,
+        KC_P1,         KC_P2,       KC_P3,         KC_P0,      KC_ENT
     )
 };
 
